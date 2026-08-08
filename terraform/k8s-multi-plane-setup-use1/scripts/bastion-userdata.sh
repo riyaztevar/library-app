8580573f5a7f2e2afeedebedcbc369d91208c200
Riyaz Tevar <riyaz_tk@yahoo.in>
2026-08-09 00:27:30 +0530

    more fix

--- terraform/k8s-multi-plane-setup-use1/scripts/bastion-userdata.sh
+++ terraform/k8s-multi-plane-setup-use1/scripts/bastion-userdata.sh
@@ -12,17 +12,16 @@ Host *
   StrictHostKeyChecking no
 EOF
 
-sudo dnf install -y awscli ansible-core python-pip >> $LOG_FILE
+sudo dnf install -y awscli python-pip >> $LOG_FILE
 aws secretsmanager get-secret-value --secret-id node_rsa_key --query SecretString --output text > /home/ec2-user/.ssh/id_rsa 2> $LOG_FILE
 sudo chown ec2-user /home/ec2-user/.ssh/id_rsa
 chmod 400 /home/ec2-user/.ssh/id_rsa
 
 sudo mkdir /var/log/ansible
 sudo chown ec2-user.ec2-user /var/log/ansible
-pip3 install boto3 botocore >> $LOG_FILE 2>&1 
-ansible-galaxy collection install community.general -p /home/ec2-user/.ansible/collections >> $LOG_FILE 2>&1
-ansible-galaxy collection install ansible.posix -p /home/ec2-user/.ansible/collections >> $LOG_FILE 2>&1
-ansible-galaxy collection install amazon.aws -p /home/ec2-user/.ansible/collections >> $LOG_FILE 2>&1
+pip3 install boto3 botocore ansible >> $LOG_FILE 2>&1
+export PATH=$PATH:/home/ec2-user/.local/bin >> ~/.bashrc
+ansible-galaxy collection install community.general ansible.posix amazon.aws >> $LOG_FILE 2>&1
 
 git clone https://github.com/riyaztevar/library-app.git >> $LOG_FILE 2>&1
 cd library-app/ansible
@@ -36,4 +35,5 @@ sudo mv kubectl /usr/local/bin/ && echo "installed kubectl cli" >> $LOG_FILE
 #copy config
 
 mkdir /home/ec2-user/.kube
+sudo chown ec2-user.ec2-user /home/ec2-user/.kube
 scp ec2-user@${CTLPLANE_IP}:/home/ec2-user/.kube/config /home/ec2-user/.kube/config >> $LOG_FILE 2>&1
