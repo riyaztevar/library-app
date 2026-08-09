e7a21be14cf8875c0430eec79c94cac6f61427f6
Riyaz Tevar <riyaz_tk@yahoo.in>
2026-08-09 11:53:53 +0530

    more fixes

--- terraform/k8s-multi-plane-setup-use1/scripts/bastion-userdata.sh
+++ terraform/k8s-multi-plane-setup-use1/scripts/bastion-userdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo fallocate -l 2G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
 
 LOG_FILE="/tmp/setup.log"
 
@@ -12,16 +12,16 @@ Host *
   StrictHostKeyChecking no
 EOF
 
-sudo dnf install -y awscli python-pip >> $LOG_FILE
+dnf install -y git awscli python-pip >> $LOG_FILE
 aws secretsmanager get-secret-value --secret-id node_rsa_key --query SecretString --output text > /home/ec2-user/.ssh/id_rsa 2> $LOG_FILE
-sudo chown ec2-user /home/ec2-user/.ssh/id_rsa
+chown ec2-user /home/ec2-user/.ssh/id_rsa
 chmod 400 /home/ec2-user/.ssh/id_rsa
 
-sudo mkdir /var/log/ansible
-sudo chown ec2-user.ec2-user /var/log/ansible
-pip3 install boto3 botocore ansible >> $LOG_FILE 2>&1
+mkdir /var/log/ansible
+chown ec2-user.ec2-user /var/log/ansible
+sudo -u ec2-user pip3 install boto3 botocore ansible >> $LOG_FILE 2>&1
 export PATH=$PATH:/home/ec2-user/.local/bin >> ~/.bashrc
-ansible-galaxy collection install community.general ansible.posix amazon.aws >> $LOG_FILE 2>&1
+sudo -u ec2-user ansible-galaxy collection install community.general ansible.posix amazon.aws >> $LOG_FILE 2>&1
 
 git clone https://github.com/riyaztevar/library-app.git >> $LOG_FILE 2>&1
 cd library-app/ansible
@@ -30,10 +30,10 @@ ansible-playbook -v playbooks/k8s.yml -e skip_setup=false > /var/log/ansible/ans
 #install kubectl client
 curl -LO "https://dl.k8s.io/release/v1.31.0/bin/linux/amd64/kubectl" || echo "failed to download kubectl binary" >> $LOG_FILE
 chmod +x kubectl
-sudo mv kubectl /usr/local/bin/ && echo "installed kubectl cli" >> $LOG_FILE
+mv kubectl /usr/local/bin/ && echo "installed kubectl cli" >> $LOG_FILE
 
 #copy config
 
 mkdir /home/ec2-user/.kube
-sudo chown ec2-user.ec2-user /home/ec2-user/.kube
+chown ec2-user.ec2-user /home/ec2-user/.kube
 scp ec2-user@${CTLPLANE_IP}:/home/ec2-user/.kube/config /home/ec2-user/.kube/config >> $LOG_FILE 2>&1
