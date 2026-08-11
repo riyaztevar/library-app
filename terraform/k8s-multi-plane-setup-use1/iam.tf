2a6e4321bb98a0c1336f02c995f5f0e744de17ab
Riyaz Tevar <riyaz_tk@yahoo.in>
2026-08-11 17:32:14 +0530

    lot of changes

--- terraform/k8s-multi-plane-setup-use1/iam.tf
+++ terraform/k8s-multi-plane-setup-use1/iam.tf
@@ -55,39 +55,51 @@ data "aws_iam_policy_document" "secrets_reader" {
   }
 }
 
-# Example of how to attach this document to a policy resource
+# Secret read policy for bastion
 resource "aws_iam_policy" "secrets_reader" {
   name        = "secrets-reader-policy"
   description = "Grants permission to read Secrets Manager secret"
   policy      = data.aws_iam_policy_document.secrets_reader.json
 }
 
-
-locals {
-  inline_policies = [
-    "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicyV2",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  ]
+# lb controller policy loaded from json file
+resource "aws_iam_policy" "lb_policy" {
+  name        = "lb_controller_policy"
+  description = "IAM policy loaded directly from the JSON file"
+  policy = file("${path.module}/lb_controller_policy.json")
 }
 
-# 3. Attach the AWS-Managed Policy to the Role
+# aws managed policies for k8s nodes
+locals {
+  inline_policies = { 
+    ebs_csi_driver_policy: "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicyV2",
+    container_registry_readonly_policy: "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    lb_controller_poliy: aws_iam_policy.lb_policy.arn,
+   cni_policy: "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  }
+}
+
+#3. Attach the AWS-Managed Policies to the Role
 resource "aws_iam_role_policy_attachment" "ebs_csidriver_policy" {
-  for_each   = toset(local.inline_policies)
+  for_each   = local.inline_policies
   role       = aws_iam_role.ec2_role.name
   policy_arn = each.value
 }
 
+
+# bastion policy attachment
 resource "aws_iam_role_policy_attachment" "bastion_policy" {
   role       = aws_iam_role.bastion_role.name
   policy_arn = aws_iam_policy.secrets_reader.arn
 }
 
+#k8s nodes instance profile
 resource "aws_iam_instance_profile" "nodes_instance_profile" {
   name = "k8s_node_instance_profile"
   role = aws_iam_role.ec2_role.name
 }
 
-
+# bastion instance profile
 resource "aws_iam_instance_profile" "bastion_instance_profile" {
   name = "bastion_instance_profile"
   role = aws_iam_role.bastion_role.name
